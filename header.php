28d7ce97e9c75b960140b1caf94b15751ccef2ba
Skywt2003 <skywt2003@gmail.com>
2021-08-04 20:44:13 +0800

    Fix&complete header&footer

--- header.php
+++ header.php
@@ -81,13 +81,13 @@
         <ul class="col navbar-nav justify-content-center">
             <li class="nav-item">
                 <a class="nav-link nav-link-icon <?php if ($this->is('index')){echo 'text-default';}else{echo 'text-gray';}?>" href="<?php $this->options->siteUrl();?>">
-                    <i class="fa fa-home"></i> Home
+                    <i class="fa fa-home"></i> <?php _e('首页') ?>
                 </a>
             </li>
             <?php if (strpos($this->options->frontPage, 'file') !== FALSE) {?>
             <li class="nav-item">
                 <a class="nav-link nav-link-icon <?php if ($this->is('archive')){echo 'text-default';}else{echo 'text-gray';}?>" href="<?php echo $this->options->siteUrl.$this->options->routingTable['archive']['url'] ?>">
-                    <i class="fa fa-book"></i> Passages
+                    <i class="fa fa-book"></i> <?php _e('文章') ?>
                 </a>
             </li>
             <?php } ?>
@@ -105,6 +105,7 @@
         </ul>
     </div>
 </nav>
+<script>$(document).ready(function(){$(".navbar ul.navbar-nav li:last-child").addClass("mr-0");});</script>
 
 <div id="body">
     <div class="container">
