91800d1baaa6549e29ba7c246b33fd6a5c1a42f7
Skywt2003 <skywt2003@gmail.com>
2021-08-06 10:05:43 +0800

    Fix icon styles

--- post.php
+++ post.php
@@ -6,12 +6,12 @@
         <h1 class="font-weight-bold post-title" itemprop="name headline"><?php $this->title() ?></h1>
         <div>
             <span class="text-gray">
-                <i class="far fa-calendar-alt"></i>
+                <i class="far fa-calendar-alt ml-1 mr-1"></i>
                 <time class="lately-a" datetime="<?php $this->date('c'); ?>" itemprop="datePublished" pubdate>Lately</time> | 
                 <time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date('Y-m-d D h:iA'); ?></time>
             </span>
             <?php if ($this->user->hasLogin()): ?>
-                <a class="badge badge-primary" href="<?php $this->options->siteUrl();?>admin/write-post.php?cid=<?php $this->cid();?>">
+                <a class="badge badge-primary ml-1 mr-1" href="<?php $this->options->siteUrl();?>admin/write-post.php?cid=<?php $this->cid();?>">
                     <i class="far fa-edit"></i>
                     <?php _e('编辑'); ?>
                 </a>
@@ -25,18 +25,18 @@
     </article>
     <hr>
     <span>
-        <i class="fa fa-copyright"></i>
+        <i class="fa fa-copyright ml-1 mr-1"></i>
         <a class="badge badge-default" rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons BY-SA 4.0</a>
     </span>
     <span>
-        <i class="fa fa-folder text-info"></i>
+        <i class="fa fa-folder text-info ml-1 mr-1"></i>
         <?php foreach($this->categories as $categories): ?>
 		<a href="<?php print($categories['permalink']) ?>" class="badge badge-info"><?php print($categories['name']) ?></a>
 	    <?php endforeach;?>
     </span>
     <?php if (count($this->tags) > 0): ?>
     <span>
-        <i class="fa fa-tags text-success"></i>
+        <i class="fa fa-tags text-success ml-1 mr-1"></i>
 	    <?php foreach($this->tags as $tags): ?>
 	    <a href="<?php print($tags['permalink']) ?>" class="badge badge-success"><?php print($tags['name']) ?></a>
 	    <?php endforeach;?>
@@ -44,7 +44,7 @@
 	<?php endif;?>
     <?php if ($this->commentsNum > 0): ?>
     <span>
-        <i class="fa fa-comments"></i>
+        <i class="fa fa-comments ml-1 mr-1"></i>
         <span class="badge badge-secondary"><?php echo $this->commentsNum?> 条评论</span>
     </span>
     <?php endif; ?>
